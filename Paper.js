73dd05aa87b1271445df8b2cbab56dbb83ab7d60
vivek <priyagole@gmail.com>
2020-12-04 16:10:05 +0530

    finishing the project

--- Paper.js
+++ Paper.js
@@ -13,7 +13,8 @@ class Paper
 		this.x=x;
 		this.y=y;
 		this.r=r
-		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options);
+		this.image = loadImage("paper.png");
 		World.add(world, this.body);
 
 	}
@@ -24,10 +25,8 @@ class Paper
 
 			push()
 			translate(paperpos.x, paperpos.y);
-			rectMode(CENTER)
-			strokeWeight(3);
-			fill(255,0,255)
-			ellipse(0,0,this.r, this.r);
+			imageMode(CENTER);
+			image(this.image,0,0,this.r, this.r);
 			pop()
 			
 	}
